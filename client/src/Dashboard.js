70dbb3c21b138894276f038cfc41236fdb511212
Arpit Soni <arpit.soni@afourtech.com>
2017-05-16 12:44:43 +0530

    Replaced chartJs with HighCharts. Added Pie chart & Bar chart.

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import {Doughnut, Bar} from 'react-chartjs-2';
 import { connect } from 'react-redux';
 import { fetchChartData } from './actions';
 import { Link } from 'react-router-dom'
+import ReactHighcharts from 'react-highcharts';
+import Highcharts from 'highcharts';
 
 const categoryList = {
   arts_entertainment : "Arts & Entertainment",
@@ -12,27 +13,87 @@ const categoryList = {
 }
 
 let dataBycategory = {
- labels: [ ],
- datasets: [{
-    data: [],
-    backgroundColor: [ "#2ecc71", "#3498db", "#95a5a6", "#9b59b6", "#f1c40f", "#e74c3c", "#34495e",
-      '#FF6384', '#36A2EB', '#FFCE56', '#800080' ],
- }]
+  chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Tasks - Categories Chart'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.y}</b>'
+    },
+    credits: {
+        enabled: false
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.y}',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Tasks',
+        colorByPoint: true,
+        data: []
+    }]
 };
 
 let allData = {
-  labels: [],
-  datasets: [
-    {
-      label: 'No. of Tasks for user',
-      backgroundColor: 'rgba(27, 165, 255, 0.4)',
-      borderColor: 'rgba(27, 165, 255, 1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(98, 247, 102, 0.4)',
-      hoverBorderColor: 'rgba(98, 247, 102, 1)',
-      data: []
-    }
-  ]
+  chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Tasks - Users Chart'
+    },
+    xAxis: {
+        categories: []
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Number of tasks'
+        },
+        stackLabels: {
+            enabled: true,
+            style: {
+                fontWeight: 'bold',
+                color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+            }
+        }
+    },
+    tooltip: {
+        headerFormat: '<b>{point.x}</b><br/>',
+        pointFormat: 'Tasks: {point.y}'
+    },
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            dataLabels: {
+                enabled: false,
+                color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+            }
+        }
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Users',
+        data: [ ]
+    }]
 };
 
 const chartOptions = {
@@ -45,93 +106,50 @@ class Dashboard extends Component {
   }
 
   render () {
-    dataBycategory.labels = []
-    dataBycategory.datasets[0].data = []
-    allData.labels = []
-    allData.datasets[0].data = []
-
     if(this.props.chartData.dataBycategory) {
+      dataBycategory.series[0].data = []
       this.props.chartData.dataBycategory.data.map( val => {
+        let name
         if(categoryList[val.category])
-          dataBycategory.labels.push( categoryList[val.category] )
+          name = categoryList[val.category]
         else
-          dataBycategory.labels.push("Others")
-          
-        dataBycategory.datasets[0].data.push(val.count)
+          name = "Others"
+
+        dataBycategory.series[0].data.push({ name: name, y :val.count })
       })
     }
 
     if(this.props.chartData.allData) {
+      allData.xAxis.categories = []
+      allData.series[0].data = []
       this.props.chartData.allData.map( val => {
-        allData.labels.push(val.userName)  
-        allData.datasets[0].data.push(val.count)
+        allData.xAxis.categories.push(val.userName)  
+        allData.series[0].data.push(Number(val.count))
       })
     }
 
     return (
       <div className="col-sm-12">
         <div id="bloggraph" className="col-sm-8">
-          <h3>Tasks - Categories </h3>
-          <h1></h1>
+          <br></br>
           { this.props.chartData.dataBycategory && this.props.chartData.dataBycategory.total ? 
-            <Doughnut data={dataBycategory} options={chartOptions}/> : "No task added yet." }
+            <ReactHighcharts config = {dataBycategory}></ReactHighcharts> : "No task added yet." }
         </div>
         <div id="bloggraph" className="col-sm-4">
-          <h1></h1>
+          <br></br>
           <p>Total task count : { this.props.chartData.dataBycategory ? this.props.chartData.dataBycategory.total : 0 }</p>
           <p><Link to="/tasks" className="">View Tasks</Link></p>
         </div>
 
         <div id="bloggraph" className="col-sm-8">
-          <h1></h1>
-          <h1></h1>
-          <h1></h1>
-          <h3>Tasks - Users</h3>
-          <h1></h1>
-          <Bar data={allData} options={chartOptions}/>
-          <h1></h1>
+          <br></br>
+          <br></br>
+          <br></br>
+          <ReactHighcharts id="test2" config = {allData}></ReactHighcharts>
+          <br></br>
         </div>
       </div>
     )
-  }
-
-  componentWillMount() {
-    Chart.pluginService.register({
-      beforeRender: function (chart) {
-        if (chart.config.options.showAllTooltips) {
-          chart.pluginTooltips = [];
-          chart.config.data.datasets.forEach(function (dataset, i) {
-            chart.getDatasetMeta(i).data.forEach(function (sector, j) {
-              chart.pluginTooltips.push(new Chart.Tooltip({
-                _chart: chart.chart,
-                _chartInstance: chart,
-                _data: chart.data,
-                _options: chart.options.tooltips,
-                _active: [sector]
-              }, chart));
-            });
-          });
-          // turn off normal tooltips
-          chart.options.tooltips.enabled = false;
-        }
-      }, afterDraw: function (chart, easing) {
-        if (chart.config.options.showAllTooltips) {
-          // we don't want the permanent tooltips to animate, so don't do anything till the animation runs atleast once
-          if (!chart.allTooltipsOnce) {
-            if (easing !== 1) return;
-            chart.allTooltipsOnce = true;
-          }
-          chart.options.tooltips.enabled = true;
-          Chart.helpers.each(chart.pluginTooltips, function (tooltip) {
-            tooltip.initialize();
-            tooltip.update(); // we don't actually need this since we are not animating tooltips
-            tooltip.pivot();
-            tooltip.transition(easing).draw();
-          });
-          chart.options.tooltips.enabled = false;
-        }
-      }
-    });
   }
 }
 
